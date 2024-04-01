ca79517526020e22812026cb19a9e9cf3d405832
mevikash <badal8core@gmail.com>
2024-04-01 13:55:57 +0530

    Update looks 54

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
 
     <div className="w-full  mx-auto p-0 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="#" className="ml-10 block xl:text-xl sm:text-sm text-white font-alexBrush sm:text-center">
+            <a href="#" className="xl:ml-10 lg:ml-10 md:ml-10 sm:ml-0 block xl:text-xl sm:text-sm text-white font-alexBrush sm:text-center">
                 <Image
                         src="/3.png"
                         alt="meter"
@@ -19,13 +19,13 @@ const Footer = () => {
                         className="lg:w-48 xl:w-48 md:w-48 sm:w-28 "
                     />
             </a>
-            <div className="sm:flex sm:items-center  sm:justify-between"> 
-            <span className="block xl:text-xl  lg:text-lg md:text-md sm:text-sm text-white font-alexBrush sm:text-center">© 2024 <a href="#" className="font-alexBrush hover:underline">Armatrix</a>. All Rights Reserved.</span>
-            </div>
+             
+            <span className="block  xl:text-xl  lg:text-lg md:text-lg sm:text-xs text-white font-alexBrush sm:text-center">© 2024 Armatrix. All Rights Reserved.</span>
+            
            
 
-            <ul className="mr-16  sm:flex lg:w-48 xl:w-48 md:w-48 sm:w-24 sm:items-center sm:justify-between">
-            <span style={{ width: "90%", height: "70%" }} className="block px-2 xl:text-xl lg:text-lg md:text-md sm:text-sm text-white  font-alexBrush">contact@armatrix.in</span>
+            <ul className="lg:mr-16 xl:mr-16 md:mr-16 sm:mr-0 sm:ml-0  lg:w-48 xl:w-48 md:w-48 ">
+            <span style={{ width: "20%", height: "50%" }} className="block lg:px-2 xl:px-2 md:px-2 sm:px-0 xl:text-xl lg:text-lg md:text-lg sm:text-xs text-white  font-alexBrush">contact@armatrix.in</span>
                 {/* <li>
                     contact@armatrix.in
                 
@@ -33,7 +33,7 @@ const Footer = () => {
             </ul>
            
         </div>
-        <hr className=" border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-2" />
+        <hr className="border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-2 sm:my-2" />
   {/* <a className="block px-2 text-gray-500 sm:text-center dark:text-gray-400 font-alexBrush" href="https://github.com/mevikash">Credit: meVikash</a> */}
     </div>
 
